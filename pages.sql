ef98b276fc537629c59a3b123e519bbf1853d171
Creditbox <61037811+creditbox123@users.noreply.github.com>
2022-09-08 16:15:30 +0700

    Update pages.sql

--- pages.sql
+++ pages.sql
@@ -10,7 +10,7 @@ CREATE TABLE `pages` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `pages` (`id`, `link`, `linklabel`, `view`, `template`, `robots`, `meta_title`, `meta_description`) VALUES
-(1, '/', 'Home', '1', 'home', 'noindex,nofollow', 'Basic CMS', 'A Basic CMS for web developers to expand Their nowledge and understanding of full-stack / Back-end Programming'),
+(1, '/', 'Home', '1', 'home', 'noindex,nofollow', 'Basic CMS', 'A Basic CMS for web developers to expand their knowledge and understanding of full-stack / Back-end Programming'),
 (2, '/about', 'About', '1', 'about', 'noindex,nofollow', 'About This CMS', 'This CMS (Content Management System) Is A fully fynamic, database driven, One Page Runs All System.'),
 (3, '/contact', 'Contact', '1', 'contact', 'noindex,nofollow', 'Contact us', 'Send us a message to https://seacry.slack.com | The Seacry Team is happy to invest in transforming your efforts into results.
 Help You Develop Your Projects'),
