f049ae018bfec076f43503d4d8f0c6587445028f
Creditbox <61037811+creditbox123@users.noreply.github.com>
2022-09-08 16:16:15 +0700

    Update pages.sql

--- pages.sql
+++ pages.sql
@@ -11,7 +11,7 @@ CREATE TABLE `pages` (
 
 INSERT INTO `pages` (`id`, `link`, `linklabel`, `view`, `template`, `robots`, `meta_title`, `meta_description`) VALUES
 (1, '/', 'Home', '1', 'home', 'noindex,nofollow', 'Basic CMS', 'A Basic CMS for web developers to expand their knowledge and understanding of full-stack / Back-end Programming'),
-(2, '/about', 'About', '1', 'about', 'noindex,nofollow', 'About This CMS', 'This CMS (Content Management System) Is A fully fynamic, database driven, One Page Runs All System.'),
+(2, '/about', 'About', '1', 'about', 'noindex,nofollow', 'About This CMS', 'This CMS (Content Management System) Is a fully fynamic, database driven, One Page Runs All System.'),
 (3, '/contact', 'Contact', '1', 'contact', 'noindex,nofollow', 'Contact us', 'Send us a message to https://seacry.slack.com | The Seacry Team is happy to invest in transforming your efforts into results.
 Help You Develop Your Projects'),
 (4, '/404', '404', '1', '404', 'noindex,nofollow', '404 Error', '404 Error | Page Not Found'),
